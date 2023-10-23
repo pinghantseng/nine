dfb01db03a39cf6bef203688d436018ba9fe048d
pinghantseng <147016708+pinghantseng@users.noreply.github.com>
2023-10-23 16:51:52 +0800

    Update hw2.c

--- HW/hw2.c
+++ HW/hw2.c
@@ -6,7 +6,7 @@ int chess[SIZE][SIZE];
 void inputArray(void) {
     for (int i = 0; i < 8; i++){
         for (int j = 0; j < 8; j++)
-            scanf("%d", &(chess[i][j]));
+            scanf("%1d", &(chess[i][j]));
     }
     return;
 }
