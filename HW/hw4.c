854f1c376a8ea4cb518ef4a0ad0291fff9f0b3c3
pinghantseng <147016708+pinghantseng@users.noreply.github.com>
2023-10-31 22:11:13 +0800

    Update hw4.c

--- HW/hw4.c
+++ HW/hw4.c
@@ -2,7 +2,7 @@
 #define SIZE 8
 #define BOUND (x+((a+1) * dx) != -1) && (x+((a+1) * dx) != 8) && (y+((a+1) * dy) != -1) && (y+((a+1) * dy) != 8)
 
-int chess[SIZE][SIZE];
+int chess[SIZE][SIZE] = {0};
 int next[2][SIZE][SIZE] = {0};
 
 void inputArray(void) {
@@ -26,11 +26,13 @@ void find(void){
                         if(dx != 0 || dy != 0){
                             for(int a = 1; a < 7; a++){
                                 if(BOUND){
-                                    if(chess[x+(a * dx)][y+(a * dy)] == antiColor)
+                                    if(chess[x+(a * dx)][y+(a * dy)] == antiColor){
                                         if(chess[x+((a+1) * dx)][y+((a+1) * dy)] == 0){
                                             next[color][x+((a+1) * dx)][y+((a+1) * dy)] = 1;
                                             break;
                                         }
+                                    }
+                                    else break;
                                 }
                                 else break;
                             }
