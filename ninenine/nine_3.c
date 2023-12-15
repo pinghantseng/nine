2f183ad2a6f6e390e4b1a4c5c3c4d719a9da1530
hanstseng <411221262@gms.ndhu.edu.tw>
2023-12-15 23:15:09 +0800

    chess upload

--- ninenine/nine_3.c
+++ ninenine/nine_3.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-    for (int k = 0; k <=6; k+=3){
+    for (int k = 0; k <= 6; k += 3){
         for (int j = 1; j <= 9; j++){
-            for (int i = 1 + k; i <= 3+k; i ++)
-                printf("%d x %d = %2d   ",i ,j ,i *j);
+            for (int i = 1 + k; i <= 3 + k; i++)
+                printf("%d x %d = %2d   ",i ,j ,i * j);
             printf("\n");
         }
     printf("\n\n");
